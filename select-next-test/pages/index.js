abdbf931208055c5a77f0e15918485724209b9ab
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2019-01-25 10:55:06 +0100

    created new react and next apps with create-react-app and create-next-app and implemented the same bug example in both

---
+++ select-next-test/pages/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myradio: undefined,
+      myselect: "one"
+    };
+  }
+
+  onChangeRadio = event => {
+    this.setState({ myradio: event.target.value });
+  };
+
+  onChangeSelect = event => {
+    this.setState({ myselect: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="radio"
+          value="one"
+          name="myradio"
+          required
+          onChange={this.onChangeRadio}
+          checked={this.state.myradio === "one"}
+        />
+        <input
+          type="radio"
+          value="two"
+          name="myradio"
+          required
+          checked={this.state.myradio === "two"}
+          onChange={this.onChangeRadio}
+        />
+        <select
+          name="myselect"
+          required
+          value={this.state.myselect}
+          onChange={this.onChangeSelect}
+        >
+          <option value="">Please choose...</option>
+          <option value="one">One</option>
+          <option value="two">Two</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default App;
